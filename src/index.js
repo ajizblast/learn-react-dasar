158aaa08abdec32c79f7ca91dbcc995e564dae07
ajizblast <ajizblast@gmail.com>
2023-06-26 11:49:05 +0700

    event

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 // import component
-import EmbedExpression from "./embedExpression";
+import Event from "./event";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <EmbedExpression />
+    <Event />
   </React.StrictMode>
 );
 
