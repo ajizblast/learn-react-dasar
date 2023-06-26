bf2b43834152921fd3851547469a8354070b3b7c
ajizblast <ajizblast@gmail.com>
2023-06-26 13:28:00 +0700

    state

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 // import component
-import Props from "./props";
+import State from "./state";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Props />
+    <State />
   </React.StrictMode>
 );
 
